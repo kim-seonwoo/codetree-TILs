573810c917852b04ab49792db5a3ced8fa8bff9a
Seon Woo Kim <95562494+kim-seonwoo@users.noreply.github.com>
2024-03-09 15:32:24 +0900

    Title: 함수를 이용한 윤년 판별, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240309/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -2,10 +2,14 @@ num = int(input())
 
 def calYear(num):
     if num % 4 == 0:
+        if num % 100 == 0 and num % 400 != 0:
+            return False
+
         return True
 
-    elif num % 100 == 0 and num % 400 != 0:
-        return True
+    return False
+
+
 
 
 if calYear(num):
