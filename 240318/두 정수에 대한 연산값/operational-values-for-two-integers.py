dfbbe47743fae88e070f41c20175fd55846ad616
Seon Woo Kim <95562494+kim-seonwoo@users.noreply.github.com>
2024-03-18 22:24:19 +0900

    Title: 두 정수에 대한 연산값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240318/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,27 @@
+num1, num2 = tuple(map(int, input().split()))
+
+def calNume(num1, num2):
+    a = 0
+    b = 0
+
+    if num1 > num2:
+        a = num1
+        b = num2
+    else: 
+        b = num1
+        a = num2
+
+
+    a += 25
+    b *= 2
+
+    num1 = b
+    num2 = a
+
+    print(num1, num2
+
+    return num1, num2
+
+calNume(num1, num2)
+
+print(num1, num2)
