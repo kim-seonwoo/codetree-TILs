3fb2ba69db09665b940787ff537eb1f310c0f5a4
Seon Woo Kim <95562494+kim-seonwoo@users.noreply.github.com>
2024-08-07 21:46:12 +0900

    Title: 떨어지는 1자 블록, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240807/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240807/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,62 +1,40 @@
-# 변수 선언 및 입력
-n, curr_x, curr_y = tuple(map(int, input().split()))
-a = [[0] * (n + 1)]
-for _ in range(n):
-    a.append([0] + list(map(int, input().split())))
+# 변수 선언 및 입력: 
 
-# 방문하게 되는 숫자들을 담을 곳입니다.
-visited_nums = []
+n, m, k = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-
-# 범위가 격자 안에 들어가는지 확인합니다.
-def in_range(x, y):
-    return 1 <= x and x <= n and 1 <= y and y <= n
-
-
-# 범위가 격자 안이고, 해당 위치의 값이 더 큰지 확인합니다.
-def can_go(x, y, curr_num):
-    return in_range(x, y) and a[x][y] > curr_num
+# 해당 row에 [col_s, col_e] 열에
+# 전부 블럭이 없는지를 확인합니다.
+def all_blank(row, col_s, col_e):
+    return all([
+        not grid[row][col]
+        for col in range(col_s, col_e + 1)
+    ])
 
 
-# 조건에 맞춰 움직여봅니다.
-# 움직였다면 true를 반환하고
-# 만약 움직일 수 있는 곳이 없었다면 false를 반환합니다.
-def simulate():
-    global curr_x, curr_y
-    
-    # 코딩의 간결함을 위해 
-    # 문제 조건에 맞게 상하좌우 순서로
-    # 방향을 정의합니다.
-    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-    
-    # 각각의 방향에 대해 나아갈 수 있는 곳이 있는지 확인합니다.
-    for dx, dy in zip(dxs, dys):
-        next_x, next_y = curr_x + dx, curr_y + dy
+# 최종적으로 도달하게 될 위치는
+# 그 다음 위치에 최초로 블럭이 존재하는 순간임을 이용합니다.
+def get_target_row():
+    for row in range(n - 1):
+        if not all_blank(row + 1, k, k + m - 1):
+            return row
+
+    return n - 1
         
-        # 갈 수 있는 곳이라면
-        # 이동하고 true를 반환합니다.
-        if can_go(next_x, next_y, a[curr_x][curr_y]):
-            curr_x, curr_y = next_x, next_y
-            return True
-    
-    # 움직일 수 있는 곳이 없었다는 의미로
-    # false 값을 반환합니다.
-    return False
 
+k -= 1
 
-# 초기 위치에 적혀있는 값을 답에 넣어줍니다.
-visited_nums.append(a[curr_x][curr_y])
-while True:
-    # 조건에 맞춰 움직여봅니다.
-    greater_number_exist = simulate()
-    
-    # 인접한 곳에 더 큰 숫자가 없다면 종료합니다.
-    if not greater_number_exist:
-        break
-    
-    # 움직이고 난 후의 위치를 답에 넣어줍니다.
-    visited_nums.append(a[curr_x][curr_y])
+# 최종적으로 멈추게 될 위치를 구합니다.
+target_row = get_target_row()
 
-# 출력:
-for num in visited_nums:
-    print(num, end=' ')
+# 최종 위치에 전부 블럭을 표시합니다.
+for col in range(k, k + m):
+    grid[target_row][col] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
+    print()
