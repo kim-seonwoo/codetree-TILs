91a15fc0ed57918af3cdb6cb30f04ee3797d7d48
Seon Woo Kim <95562494+kim-seonwoo@users.noreply.github.com>
2024-08-08 14:56:58 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240808/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240808/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -16,7 +16,7 @@ def find_permutations(cnt):
     
     # 1부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
     for i in range(1, k + 1):
-        if cnt => 2 and selected_nums[-1] != i and selected_nums[-2] != i:
+        if cnt => 2 and selected_nums[-1] == i and selected_nums[-2] == i:
             continue
         else:
             selected_nums.append(i)
