5afd88423d1e7f314ee9018322cebad6241ed948
Seon Woo Kim <95562494+kim-seonwoo@users.noreply.github.com>
2024-08-09 20:56:05 +0900

    Title: 최대 증가 부분 수열, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,20 @@
+n = int(input())
+list_num = tuple(map(int, input().split()))
+dp = []
+count = 1
+
+def calList(index):
+    global count
+
+    if index == n:
+        return
+
+    if list_num[index] > dp[-1]:
+        dp.append(list_num[index])
+        count += 1
+        calList(index + 1)
+
+dp.append(list_num[0])
+calList(list_num[0])
+
+print(count + 1)
