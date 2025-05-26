c052c0a7f4d99d2c6eaffb73037b345604933a58
Seon Woo Kim <95562494+kim-seonwoo@users.noreply.github.com>
2025-05-26 23:42:15 +0900

    Title: 양수 직사각형의 최대 크기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250526/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 250526/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,30 +1,21 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
+max_area = 0
 
-max_num = 0
-
-def cal(x, y, plus_i, plus_j):
-    result = 0
-    for i in range(x, plus_i + 1):
-        for j in range(y, plus_j + 1):
+def is_valid(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
             if grid[i][j] < 0:
-                return -1
-            else:
-                result += 1
-
-    return (plus_i + 1 - x) * (plus_j + 1 - y)
-
+                return False
+    return True
 
 for i in range(n):
     for j in range(m):
-        for num_i in range(i, n):
-            for num_j in range(j, m):
-                max_num = max(max_num, cal(i, j, num_i, num_j))
+        for ni in range(i, n):
+            for nj in range(j, m):
+                if is_valid(i, j, ni, nj):  # 여기서만 넓이 계산
+                    area = (ni - i + 1) * (nj - j + 1)
+                    max_area = max(max_area, area)
 
-
-if max_num == -1:
-    print(-1)
-else:
-    print(max_num)
+print(max_area if max_area > 0 else -1)
